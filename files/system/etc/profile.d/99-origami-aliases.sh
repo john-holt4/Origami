ad1bd88dc0a78ff0d7afd611061fb6c5fcc0ce67
John Holt <jholtiv@gmail.com>
2025-10-23 23:19:13 +0700

    Clean up profile.d by removing unused aliases
    Removed eza aliases and starship prompt initialization.

--- files/system/etc/profile.d/99-origami-aliases.sh
+++ files/system/etc/profile.d/99-origami-aliases.sh
@@ -2,20 +2,9 @@
 
 # This file is automatically loaded by your shell
 
-# --- Eza Aliases ---
-alias ls='eza --icons --group-directories-first'
-alias ll='eza -l --icons --git --group-directories-first'
-alias l.='eza -la --icons --git --group-directories-first'
-alias tree='eza --tree'
-
 # --- Podman/Docker Aliases ---
 alias docker='podman'
 alias docker-compose='podman-compose'
-
-# --- Enable Starship Prompt ---
-if command -v starship &> /dev/null && [ -n "$PS1" ]; then
-  eval "$(starship init bash)"
-fi
 
 # --- Enable FZF Keybindings & Completion ---
 if [ -f "/usr/share/fzf/key-bindings.bash" ]; then

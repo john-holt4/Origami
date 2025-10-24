ea21fc7b240bd97bd8a3af157fe978b98f620b10
John Holt <jholtiv@gmail.com>
2025-10-24 17:32:05 +0700

    added packages

--- files/system/etc/profile.d/99-origami-aliases.sh
+++ files/system/etc/profile.d/99-origami-aliases.sh
@@ -6,5 +6,43 @@
 alias docker='podman'
 alias docker-compose='podman-compose'
 
+alias cat='bat'
+
+# eza aliases
+alias l='eza'
+alias ls='eza'                 # A "safe" override for interactive use
+alias ll='eza -l'              # long list
+alias la='eza -la'             # long list, all files
+alias l.='eza -la --ignore=.git' # A good one for projects
+alias lt='eza --tree --level=2'  # tree view
+
 # Set up fzf key bindings and fuzzy completion
 eval "$(fzf --bash)"
+
+eval "$(zoxide init bash)"
+
+#
+# Nag user to use 'fd' instead of 'find'
+#
+find() {
+  # This prints the message to your terminal's error output (stderr)
+  # so it doesn't interfere with the command's real output.
+  echo "💡 Tip: Try using 'fd' next time for a simpler and faster search." >&2
+
+  # This executes the *real* find command with all your original arguments
+  command find "$@"
+}
+
+#
+# Nag user to use 'rg' instead of 'grep'
+#
+grep() {
+  # This checks if stderr is an interactive terminal
+  # so the message won't show up in scripts.
+  if [[ -t 2 ]]; then
+    echo "💡 Tip: Try using 'rg' for a simpler and faster search." >&2
+  fi
+
+  # Execute the real grep command
+  command grep "$@"
+}

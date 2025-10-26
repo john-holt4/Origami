614dd14e681b27daffc7341bb6e79092bf7c08d4
John Holt <jholtiv@gmail.com>
2025-10-26 13:54:44 +0700

    fix typos

--- files/scripts/skel-append.sh
+++ files/scripts/skel-append.sh
@@ -1,4 +1,4 @@
-alias fastfetch='fastfetch -l /usr/share/fastfetch/presets/origami/origami-ascii.txt --logo-color-1 blue -c/usr/share/fastfetch/presets/origami/origami-fastfetch.jsonc'
+alias fastfetch='fastfetch -l /usr/share/fastfetch/presets/origami/origami-ascii.txt --logo-color-1 blue -c /usr/share/fastfetch/presets/origami/origami-fastfetch.jsonc'
 
 # --- Podman/Docker Aliases ---
 alias docker='podman'
@@ -21,28 +21,16 @@ eval "$(zoxide init bash)"
 
 eval "$(starship init bash)"
 
-#
-# Nag user to use 'fd' instead of 'find'
-#
-find() {
-  # This prints the message to your terminal's error output (stderr)
-  # so it doesn't interfere with the command's real output.
-  echo "💡 Tip: Try using 'fd' next time for a simpler and faster search." >&2
-
-  # This executes the *real* find command with all your original arguments
-  command find "$@"
+# --- Nag user to use 'fd' instead of 'find' ---
+function find {
+    printf '💡 Tip: Try using "fd" next time for a simpler and faster search.\n' >&2
+    command find "$@"
 }
 
-#
-# Nag user to use 'rg' instead of 'grep'
-#
-grep() {
-  # This checks if stderr is an interactive terminal
-  # so the message won't show up in scripts.
-  if [[ -t 2 ]]; then
-    echo "💡 Tip: Try using 'rg' for a simpler and faster search." >&2
-  fi
-
-  # Execute the real grep command
-  command grep "$@"
+# --- Nag user to use 'rg' instead of 'grep' ---
+function grep {
+    if [ -t 2 ]; then
+        printf '💡 Tip: Try using "rg" for a simpler and faster search.\n' >&2
+    fi
+    command grep "$@"
 }
